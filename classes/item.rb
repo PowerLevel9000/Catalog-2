a27bffbe398524b59cc827d3f67a6a43a14cbf1b
PowerLevel9000 <116451144+PowerLevel9000@users.noreply.github.com>
2023-05-04 12:28:34 +0530

    rubocop error

--- classes/item.rb
+++ classes/item.rb
@@ -1,23 +1,24 @@
 require 'date'
 
 class Item
-    attr_accessor :genre, :author, :source, :label, :publish_date
-    attr_reader :id, :archived
-    def initialize(date)
-        @id = rand(1..1000)
-        @publish_date = date
-        @archived = false
-    end
+  attr_accessor :genre, :author, :source, :label, :publish_date
+  attr_reader :id, :archived
 
-    def move_to_archive
-        if can_be_archived?
-            @archived = true
-        end
-    end
+  def initialize(date)
+    @id = rand(1..1000)
+    @publish_date = date
+    @archived = false
+  end
 
-    private
+  def move_to_archive
+    return unless can_be_archived?
 
-    def can_be_archived?
-        (Time.now.year - Date.parse(@publish_date).year) > 10
-    end
+    @archived = true
+  end
+
+  private
+
+  def can_be_archived?
+    (Time.now.year - Date.parse(@publish_date).year) > 10
+  end
 end
