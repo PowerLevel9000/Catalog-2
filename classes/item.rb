d86177fe511332212dd58ebc79cba0fbd670ad45
PowerLevel9000 <116451144+PowerLevel9000@users.noreply.github.com>
2023-05-04 12:25:24 +0530

    Unit testing along with  move to archive

--- classes/item.rb
+++ classes/item.rb
@@ -1,8 +1,23 @@
+require 'date'
+
 class Item
     attr_accessor :genre, :author, :source, :label, :publish_date
     attr_reader :id, :archived
     def initialize(date)
         @id = rand(1..1000)
         @publish_date = date
+        @archived = false
+    end
+
+    def move_to_archive
+        if can_be_archived?
+            @archived = true
+        end
+    end
+
+    private
+
+    def can_be_archived?
+        (Time.now.year - Date.parse(@publish_date).year) > 10
     end
 end
