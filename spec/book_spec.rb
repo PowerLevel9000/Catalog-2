c9600dbb7d79ec4f0f94848ae8abcea20c57c8a0
PowerLevel9000 <116451144+PowerLevel9000@users.noreply.github.com>
2023-05-06 23:23:23 +0530

    fix test case

--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 describe Book do
-  let(:book) { Book.new('Adarsh', 'bad') }
+  let(:book) { Book.new('Adarsh', 'bad', "2000/10/22") }
 
   it 'should be instance of book' do
     expect(book).to be_instance_of(Book)
